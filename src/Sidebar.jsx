8e9f07cad34a127a9add4eea8989c41e66ccdefd
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-27 00:02:06 +0300

    coode

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './sidebar.css'
+import SidebarRow from './SidebarRow'
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       hello this is a Sidebar
+      <SidebarRow title='Home'/>
+      <SidebarRow title='Trending'/>
+      <SidebarRow title='Subscription'/>
+      
     </div>
   )
 }
