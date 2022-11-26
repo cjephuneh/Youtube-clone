af165b024ef9acb7454d4503ea81fd573fcc0832
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-26 18:51:34 +0300

    more-code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './Header'
 function App() {
   return (
     <div className="App">
-      <h1>Hello caleb lets build a Youtube-clone</h1>
       <Header />
     </div>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Header.css'
+import Logo from './assets/logo.png'
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import VideoCallSharpIcon from '@mui/icons-material/VideoCallSharp';
 
 function Header() {
   return (
-    <div>
-      <h1>hallo guten morgen caleg + guten tag</h1>
+    <div className='header'>
+      <h1>this is a header</h1>
+      <MenuSharpIcon />
+      <VideoCallSharpIcon />
+      <img src={Logo} alt='' className='header__logo'/>
     </div>
   )
 }
