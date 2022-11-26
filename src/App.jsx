204e454e3c10d409aeef029a7e2329b1e66767d2
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-26 18:15:25 +0300

    code-more

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello caleb lets build a Youtube-clone</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>hallo guten morgen caleg + guten tag</h1>
+    </div>
+  )
+}
+
+export default Header
