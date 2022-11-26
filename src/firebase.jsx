48f0cd520850fff04101d3d29538dc4f24bd44fd
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-26 17:50:56 +0300

    first commit

---
+++ src/firebase.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1ieGyXivTuH-7spJhSKFhnS1LSjJIGeo",
+    authDomain: "clone-b0c56.firebaseapp.com",
+    projectId: "clone-b0c56",
+    storageBucket: "clone-b0c56.appspot.com",
+    messagingSenderId: "797325184701",
+    appId: "1:797325184701:web:304f9c2c605967071a71d4",
+    measurementId: "G-GV2KZWBNQ5"
+  };
