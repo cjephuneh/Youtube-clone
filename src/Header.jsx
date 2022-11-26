20d767ab32a10d635da9fb6be3d214cdf89000dd
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-26 21:28:51 +0300

    code

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,29 @@ import './Header.css'
 import Logo from './assets/logo.png'
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 import VideoCallSharpIcon from '@mui/icons-material/VideoCallSharp';
+import SearchIcon from '@mui/icons-material/Search';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   return (
     <div className='header'>
-      <h1>this is a header</h1>
-      <MenuSharpIcon />
-      <VideoCallSharpIcon />
-      <img src={Logo} alt='' className='header__logo'/>
+        <div className='header__left'>
+            <MenuSharpIcon />
+            <img src={Logo} alt='' className='header__logo'/>
+        </div>
+        <div className='header__input'>
+            <input type="text" />
+            <SearchIcon />
+        </div>
+        <div className='header__icons'>
+            <VideoCallSharpIcon />
+            <AppsOutlinedIcon />
+            <NotificationsIcon />
+            <AccountCircleIcon />
+        </div>
+       
     </div>
   )
 }
