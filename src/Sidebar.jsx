b3bded28c786ab68564896cf1c3476486332b10f
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-28 01:46:10 +0300

    more-code

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,6 +4,12 @@ import SidebarRow from './SidebarRow'
 import HomeIcon from '@mui/icons-material/Home';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import ContentCutIcon from '@mui/icons-material/ContentCut';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function Sidebar() {
   return (
@@ -11,7 +17,14 @@ function Sidebar() {
       <SidebarRow  Icon={HomeIcon} title='Home'/>
       <SidebarRow  Icon={WhatshotIcon} title='Trending'/>
       <SidebarRow Icon={SubscriptionsIcon} title='Subscription'/>
-      
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title='Library'/>
+      <SidebarRow Icon={HistoryIcon} title=' History'/>
+      <SidebarRow Icon={OndemandVideoIcon} title='Your videos'/>
+      <SidebarRow Icon={WatchLaterIcon} title='Watch Later'/>
+      <SidebarRow Icon={ContentCutIcon} title='Your clips'/>
+      <SidebarRow Icon={KeyboardArrowDownIcon} title='Show more'/>
+      <hr />
     </div>
   )
 }
