b9b3685614f85ff6101d862745dfb44fdc59aeb0
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-28 18:58:52 +0300

    code

--- src/Recommended.jsx
+++ src/Recommended.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './recommended.css'
+import VideoCard from './VideoCard'
 
 function Recommended() {
   return (
     <div className='recommended'>
       <h2>Recommended</h2>
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
     </div>
   )
 }
--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
 
-function VideoCard() {
+function VideoCard({ image, title, channel, timestamp, views, channelImage}) {
   return (
-    <div>
-      
+    <div className='videoCard'>
+       <img src={image} alt="" />
+        <div className='video__info'>
+            <Avatar 
+                className='videoCard__avatar'
+                alt={channel}
+                scr={channelImage}
+            />
+
+            <div className="video__text">
+                <h4>{title}</h4>
+                <p>{channel}</p>
+                <p>
+                    {views} . {timestamp}
+                </p>
+
+            </div>
+
+
+        </div>
     </div>
   )
 }
