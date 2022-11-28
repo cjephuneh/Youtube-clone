81573ae6fa8734658689c770986c738063870fe8
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-28 23:03:06 +0300

    niiice

--- src/Recommended.jsx
+++ src/Recommended.jsx
@@ -6,13 +6,105 @@ function Recommended() {
   return (
     <div className='recommended'>
       <h2>Recommended</h2>
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+      <VideoCard
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ"
+       />
+      <VideoCard 
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ"
+      />
+      <VideoCard 
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ"
+      />
+      <VideoCard
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ" 
+      />
+      <VideoCard 
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ"
+      />
+      <VideoCard 
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ"
+      />
+      <VideoCard
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ" 
+      />
+      <VideoCard
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ" 
+      />
+      <VideoCard
+        title="Portugal vs Uruguay 1-0 − All Gоals & Extеndеd Hіghlіghts | World Cup 2022 HD"
+        views="1.7k Views"
+        timestamp="18 minutes ago"
+        channelImage="https://yt3.ggpht.com/FZ-J_J8Dr9o9Q0b---HztM_iI1ZqdTvB
+                      74-1RD4Dy-AtgTFn04BkKd5hXpoRJR9OVpMRfmQ_3Q=s68-c-k-c0x00
+                      ffffff-no-rj"
+        image="https://i.ytimg.com/vi/RtXDw9soDQs/hq720.jpg?sqp=
+              -oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs
+              =AOn4CLAa_Y-dyhJ5oGjUOs-kEto_5FRIZQ" 
+      />
     </div>
   )
 }
