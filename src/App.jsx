e924466095f58e576f2d21fd8c3cc59552b03ed3
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-29 04:00:43 +0300

    code

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import Header from './Header'
 import Sidebar from './Sidebar'
 import RecommendedVideos from './Recommended'
 
-
 function App() {
   return (
     <div className="App">
-    
       <Header />
       <div className='app__page'>
         <Sidebar />
-        <RecommendedVideos />
-        
+        <RecommendedVideos />       
       </div>
     </div>
   )
